a660faf4d668acd1b1764bb5d4b1d1ad1a41698f
karl <72425445+kanep3@users.noreply.github.com>
2022-08-08 17:56:41 +0300

    Google Analytics

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import ReactGA from 'react-ga';
+
+const TRACKING_ID = "G-5ZNGQ12FJJ";
+ReactGA.initialize(TRACKING_ID);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
