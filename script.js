c72a8a226ac6103307e3131c521f6f0b2ba1c6da
karl <karliskanepe@icloud.com>
2023-11-01 03:32:48 +0000

    main part

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
-// Testing
+/* -- Cursor glow effect -- */
 
+const blob = document.getElementById("blob");
+
+window.onpointermove = event => { 
+  const { clientX, clientY } = event;
+  
+  blob.animate({
+    left: `${clientX}px`,
+    top: `${clientY}px`
+  }, { duration: 6969, fill: "forwards" });
+}
